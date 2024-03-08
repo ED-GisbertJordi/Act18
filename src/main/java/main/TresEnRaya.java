f50a8f244174cec2b4464f46840e237bee79f50e
Jota-GxG <jota.gvg@gmail.com>
2024-03-08 01:09:00 +0100

    Clases

--- src/main/java/main/TresEnRaya.java
+++ src/main/java/main/TresEnRaya.java
@@ -2,15 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.tresenraya;
+package main;
+
+import java.util.Scanner;
 
 /**
  *
- * @author Usuario
+ * @author Jordi Gisbert Ferriz
  */
 public class TresEnRaya {
-
+    
+    private static Scanner teclado;
+    
+    
     public static void main(String[] args) {
+        teclado = new Scanner(System.in);
+        
         System.out.println("Hello World!");
     }
 }
