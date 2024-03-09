83b350656211d0d6da4eb749325b1b62ee0a0431
Jota-GxG <jota.gvg@gmail.com>
2024-03-09 06:38:52 +0100

    Implementado selección aleatoria de icono, Terminado

--- src/main/java/juego/Simbolos.java
+++ src/main/java/juego/Simbolos.java
@@ -4,6 +4,7 @@
  */
 package juego;
 
+import java.util.Random;
 import main.TresEnRaya;
 
 /**
@@ -20,22 +21,23 @@ public class Simbolos {
     }
     
     public char obtenerSimbolo(Ficha ficha){
-        return (ficha.equals(Ficha.equis))? packs[this.seleccionado-1][0] : packs[this.seleccionado-1][1];
+        return (ficha.equals(Ficha.equis))? this.packs[this.seleccionado-1][0] : this.packs[this.seleccionado-1][1];
     }
     
     public void seleccionar(){
         System.out.print("Vamos a seleccionar los iconos con los que jugar");
-        for (int y = 0; y < packs.length; y++) {
+        for (int y = 0; y < this.packs.length; y++) {
             System.out.print("\n"+(y+1)+")");
-            for (int x = 0; x < packs[y].length; x++) {
-                System.out.print(" "+packs[y][x]);
+            for (int x = 0; x < this.packs[y].length; x++) {
+                System.out.print(" "+this.packs[y][x]);
             }
         }
-        System.out.println("\n"+(packs.length+1)+") Aleatorio");
+        System.out.println("\n"+(this.packs.length+1)+") Aleatorio");
         do {
-            int op = pedirInt("Selecciona una opción [1-"+(packs.length+1)+"]: ", "Debe introducir un entero.");
-            if (enRango(op, 1, packs.length+1)) {
+            int op = pedirInt("Selecciona una opción [1-"+(this.packs.length+1)+"]: ", "Debe introducir un entero.");
+            if (enRango(op, 1, this.packs.length+1)) {
                 this.seleccionado = op;
+                if (this.seleccionado==this.packs.length+1)    this.seleccionado = intAleatorio(1, this.packs.length);
                 break;
             }else{
                 System.out.println("Debe introducir un valor dentro del rango establecido.");
@@ -43,6 +45,11 @@ public class Simbolos {
         } while (true);
         
         
+    }
+    
+    private static int intAleatorio(int min, int max){
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
     }
     
     private boolean enRango(int a, int min, int max){
